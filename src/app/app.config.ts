8cf50e39536943566936c366b8471b2921b2a0d6
Yuri Hazhala <yuriy.gazhala@gmail.com>
2026-03-01 20:19:57 +0200

    enable PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideIndexedDb } from 'ngx-indexed-db';
 import { provideNativeDateAdapter } from '@angular/material/core';
@@ -7,6 +7,7 @@ import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { routes } from './app.routes';
 import { dbConfig } from './database/db-config';
 import { appLocale } from './utils/app-locale';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideIndexedDb(dbConfig),
     provideNativeDateAdapter(),
     { provide: MAT_DATE_LOCALE, useValue: appLocale },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
